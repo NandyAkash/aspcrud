7788b0ee62650358e7b68da9c9b703966bc6b5bd
Md Noyon Hossen <noyonhossen1996@gmail.com>
2022-05-21 17:47:20 +0600

    first commit

---
+++ WebApp1/Models/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp1.Models
+{
+    public class Employee
+    {
+        public int EmployeeID { get; set; }
+
+        public string EmployeeName { get; set; }
+
+        public string Department { get; set; }
+
+        public string Dateofjoin { get; set; }
+
+        public string photofileName { get; set; }
+    }
+}
